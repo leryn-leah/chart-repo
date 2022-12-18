24c9cbc4fe258dc271a1ed378831763beff271bd
leryn <pinktin@sina.com>
2022-12-18 15:29:36 +0800

    Specify minio chart at 4.0.14

--- customize/oss/install.sh
+++ customize/oss/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 helm install minio minio/minio -n oss \
+  --version 4.0.14 \
   --set consoleIngress.enabled=true                    \
   --set consoleIngress.hosts={oss-console.leryn.top}   \
   --set consoleIngress.ingressClassName=nginx          \
@@ -13,7 +14,7 @@ helm install minio minio/minio -n oss \
   --set persistence.size=5Gi                           \
   --set replicas=1                                     \
   --set resources.requests.memory=128Mi                \
-  --set rootPassword=xxxxxxxxxx                        \
+  --set rootPassword=W0X1angTa0                        \
   --set rootUser=admin                                 \
   --set securityContext.fsGroup=0                      \
   --set securityContext.runAsUser=0                    \
